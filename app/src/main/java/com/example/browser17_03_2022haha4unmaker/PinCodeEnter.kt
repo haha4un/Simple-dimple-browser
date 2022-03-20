e76842a8f46ec68ccc2d0f0eae92fc667b5b5409
haha4un <savelijstulev3872@gmail.com>
2022-03-20 12:29:10 +0500

    pin-code v1

--- app/src/main/java/com/example/browser17_03_2022haha4unmaker/PinCodeEnter.kt
+++ app/src/main/java/com/example/browser17_03_2022haha4unmaker/PinCodeEnter.kt
@@ -6,6 +6,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.strictmode.SqliteObjectLeakedViolation
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,7 +15,7 @@ import kotlin.random.Random
 
 class PinCodeEnter : AppCompatActivity() {
 
-    var name: Int = 0;
+    var name: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pin_code_enter)
@@ -31,23 +32,31 @@ class PinCodeEnter : AppCompatActivity() {
         var nine: Button = findViewById(R.id.nine)
         var zero: Button = findViewById(R.id.zero)
 
-        var pin: String = "";
+        var pin: String = ""
         var max: Int = 0
 
-        name = Random.nextInt(0,1000) + Random.nextInt(1000, 10000) + Random.nextInt(100, 60000)
+        name = Random.nextInt(0, 1000) + Random.nextInt(1000, 10000) + Random.nextInt(100, 60000)
 
         var db: SQLiteDatabase = baseContext.openOrCreateDatabase("users.db", MODE_PRIVATE, null)
+        db.execSQL("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL,name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
 
-        if(checkName(db, name) == name) {
+        if (checkOnName(db) == true)
+        {
+            text.text = "Надо создать пин-код. \nЗапомните его, а то придется переустанавливать приложение"
+            var opens = 1;
+            var id = 1;
 
-            text.text = "Вам надо создать пин-код \nЗапомните его, а то предется переустанавливать приложение"
 
             one.setOnClickListener() {
                 max++
                 if (max <= 4)
                     pin += "1"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             two.setOnClickListener() {
@@ -55,7 +64,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "2"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             three.setOnClickListener() {
@@ -63,7 +76,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "3"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             four.setOnClickListener() {
@@ -71,7 +88,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "4"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             five.setOnClickListener() {
@@ -79,7 +100,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "5"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             six.setOnClickListener() {
@@ -87,7 +112,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "6"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             seven.setOnClickListener() {
@@ -95,7 +124,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "7"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             eight.setOnClickListener() {
@@ -103,7 +136,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "8"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             nine.setOnClickListener() {
@@ -111,7 +148,11 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "9"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
                 text.text = pin;
             }
             zero.setOnClickListener() {
@@ -119,210 +160,227 @@ class PinCodeEnter : AppCompatActivity() {
                 if (max <= 4)
                     pin += "0"
                 else
+                {
+                    var pinTo = pin.toInt()
+                    db.execSQL("INSERT INTO user (id, name, opens, pin) VALUES ($id, $name, $opens, $pinTo)")
                     pinIsComplete(text,pin)
+                }
+                text.text = pin;
+            }
+        }
+        else
+        {
+            text.text = "Введите ваш пин-код"
+            addOpen(db)
+
+            one.setOnClickListener() {
+                max++
+                if (max <= 4)
+                    pin += "1"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "1"
+                    }
+                }
+                text.text = pin;
+            }
+            two.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "2"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "2"
+                    }
+                }
+                text.text = pin;
+            }
+            three.setOnClickListener() {
+                max++
+                if (max <= 4)
+                    pin += "3"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "3"
+                    }
+                }
+                text.text = pin;
+            }
+            four.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "4"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "4"
+                    }
+                }
+                text.text = pin;
+            }
+            five.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "5"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "5"
+                    }
+                }
+                text.text = pin;
+            }
+            six.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "6"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "6"
+                    }
+                }
+                text.text = pin;
+            }
+            seven.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "7"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "7"
+                    }
+                }
+                text.text = pin;
+            }
+            eight.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "8"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "8"
+                    }
+                }
+                text.text = pin;
+            }
+            nine.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "9"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin += "9"
+                    }
+                }
+                text.text = pin;
+            }
+            zero.setOnClickListener(){
+                max++
+                if (max <= 4)
+                    pin += "0"
+                else
+                {
+                    if (checkPin(db, pin.toInt()) == true)
+                        pinIsComplete(text, pin)
+                    else{
+                        text.text = "Пароль не подходит!"
+                        pin = "0"
+                    }
+                }
                 text.text = pin;
             }
         }
 
-        else
+    }
+
+    fun checkOnName(db: SQLiteDatabase) : Boolean
     {
-        text.text = "Введите ваш пин-код"
+        db.execSQL("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL,name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
 
-        one.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "1"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        two.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "2"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        three.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "3"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        four.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "4"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        five.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "5"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        six.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "6"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        seven.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "7"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        eight.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "8"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        nine.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "9"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-        zero.setOnClickListener() {
-            max++
-            if (max <= 4)
-                pin += "0"
-            else if (pin.toInt() == checkOnRightPin(db, checkName(db)))
-                pinIsComplete(text, pin)
-            text.text = pin;
-        }
-    }
-        db.close()
+        var check: Cursor = db.rawQuery("SELECT id FROM user", null)
+
+        var vs: Int;
+
+        var count: Int = 0
+        if (check.moveToNext())
+            count++
+
+        if (count == 0)
+            return true
+        else
+            return false
     }
 
-    fun checkOnRightPin(db: SQLiteDatabase, name: Int) : Int {
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
 
-        var check: Cursor = db.rawQuery("SELECT pin FROM user WHERE name = $name", null)
+    fun addOpen(db: SQLiteDatabase)
+    {
+        db.execSQL("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL,name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
 
-        var pin = 0;
+        var check: Cursor = db.rawQuery("SELECT opens FROM user", null)
+
+        var open = 1
         while (check.moveToNext())
         {
-            pin = check.getInt(0)
+            open = check.getInt(0)+1;
+            db.execSQL("UPDATE user SET id = $open")
         }
-        return pin
+    }
+    fun checkPin(db: SQLiteDatabase, pin: Int) : Boolean
+    {
+        db.execSQL("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL,name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
+
+        var check: Cursor = db.rawQuery("SELECT PIN FROM user", null)
+
+        while (check.moveToNext())
+        {
+            if (pin == check.getInt(0))
+                return true
+        }
+        return false
     }
 
 
     fun pinIsComplete(text: TextView, pin: String)
     {
-        checkerOnFirstEnter(text, pin)
         var note: Toast = Toast.makeText(this, "Пин-код создан. Ваш пинкод: $pin", Toast.LENGTH_LONG)
         note.show()
 
+
         var toNext: Intent = Intent(this, MainActivity::class.java)
         startActivity(toNext)
-    }
-
-    fun checkOnOpen(db: SQLiteDatabase, name: Int) : Int
-    {
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
-        var check: Cursor = db.rawQuery ("SELECT opens FROM user WHERE name = $name", null)
-
-        var opens = 0
-        while (check.moveToNext())
-        {
-            opens = check.getInt(0)
-        }
-        db.close()
-        check.close()
-
-        return opens
-    }
-    fun checkOnOpen(db: SQLiteDatabase) : Int
-    {
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
-        var check: Cursor = db.rawQuery ("SELECT opens FROM user", null)
-
-        var opens = 0
-        while (check.moveToNext())
-        {
-            opens = check.getInt(0)
-        }
-        db.close()
-        check.close()
-
-        return opens
-    }
-
-    fun checkName(db: SQLiteDatabase) : Int{
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
-        var check: Cursor = db.rawQuery ("SELECT name FROM user", null)
-
-        var name = 0
-        while (check.moveToNext())
-        {
-            name = check.getInt(0)
-        }
-        db.close()
-        check.close()
-
-        return name
-    }
-    fun checkName(db: SQLiteDatabase, name: Int) : Int{
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL UNIQUE, opens INTEGER NOT NULL DEFAULT 0, PIN NOT NULL);")
-        var check: Cursor = db.rawQuery ("SELECT name FROM user", null)
-
-        while (check.moveToNext())
-        {
-            if (name == check.getInt(0))
-                return name
-        }
-        db.close()
-        check.close()
-
-        return 0
-    }
-
-    fun checkerOnFirstEnter(text: TextView, pin: String)
-    {
-        var db: SQLiteDatabase = baseContext.openOrCreateDatabase("users.db", MODE_PRIVATE, null)
-
-        //db.execSQL("CREATE TABLE IF NOT EXISTS opens (open INTEGER, id INTEGER UNIQUE)")
-        //db.execSQL("INSERT INTO opens (id, open) VALUES (1, $open)")
-
-
-        var name = checkName(db, name)
-
-        db = baseContext.openOrCreateDatabase("users.db", MODE_PRIVATE, null)
-
-        var pinCode = pin.toInt()
-        var open: Int = checkOnOpen(db, name)
-        //
-        db = baseContext.openOrCreateDatabase("users.db", MODE_PRIVATE, null)
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS user (name INTEGER NOT NULL DEFAULT 'empty', opens INTEGER NOT NULL DEFAULT 0, PIN INTEGER NOT NULL, UNIQUE(name));")
-
-        open = checkOnOpen(db, name) +1;
-
-        db = baseContext.openOrCreateDatabase("users.db", MODE_PRIVATE, null)
-
-        db.execSQL("INSERT INTO user (name, opens, pin) VALUES ($name, $open, $pinCode)")
-
-        db.execSQL("UPDATE user SET opens = $open WHERE name = $name")
-
-        db.close()
     }
 }
